4714f7776caeedfbbdf3fded8f022cbab2b825a5
rachelxx03 <53119641+rachelxx03@users.noreply.github.com>
2024-03-03 09:54:51 -0400

    sercurity

--- llm_retrival/llm_retrival/settings.py
+++ llm_retrival/llm_retrival/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'call_llm',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
+
+]
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'llm_retrival.urls'
